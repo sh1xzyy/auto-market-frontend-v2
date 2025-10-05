f666e870a6c386a27ad8ed4abe4264a8e12602fb
sh1xzyy <alekslungol@gmail.com>
2025-10-05 12:52:36 +0300

    feat: add auth tabs

--- src/modules/AuthCard/AuthCard.jsx
+++ src/modules/AuthCard/AuthCard.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
+import AuthTabs from './ui/AuthTabs'
 
 const AuthCard = () => {
-	return <div>AuthCard</div>
+	const [activeIndex, setActiveIndex] = useState(0)
+
+	return (
+		<div>
+			<div className='mb-xl-plus bg-black-soft-bg border border-grey-deep md:rounded-lg'>
+				<AuthTabs activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+			</div>
+		</div>
+	)
 }
 
 export default AuthCard
