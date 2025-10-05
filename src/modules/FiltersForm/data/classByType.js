b8bb7a3c6241ba62f60d329c6b983ee80788a1c7
sh1xzyy <alekslungol@gmail.com>
2025-10-05 17:42:44 +0300

    feat: add class by type

---
+++ src/modules/FiltersForm/data/classByType.js
@@ -0,0 +1,12 @@
+export const classByType = {
+	category_motorBike: 'col-span-2 md:col-span-1',
+	category_eBike: 'col-span-2 md:col-span-1',
+	miniTruckCategory: 'col-span-2 md:col-span-1',
+	pricing_other_vehicles: 'col-span-1',
+	checkbox: 'col-span-2 sm:col-span-4 md:col-span-1',
+	paymentMethod_passenger: 'col-span-2 md:col-span-1',
+	submit:
+		'col-span-2 sm:order-1 md:col-span-1 md:col-start-4 md:row-start-2 md:self-end',
+	filterButton:
+		'flex justify-center col-span-2 md:cols-span-1 md:col-start-3 md:row-start-3 md:justify-self-end gap-3xl',
+}
