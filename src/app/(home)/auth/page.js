8decf64a3e458ebb88b1cb527eb0555ba996504a
sh1xzyy <alekslungol@gmail.com>
2025-10-05 12:14:52 +0300

    feat: add auth component

--- src/app/(home)/auth/page.js
+++ src/app/(home)/auth/page.js
@@ -1,5 +1,14 @@
+import Auth from '@/components/Auth/Auth'
+import Container from '@/components/Container/Container'
+
 const Page = () => {
-	return <div>Page</div>
+	return (
+		<section className='my-xl-plus md:my-4xl'>
+			<Container className='md:px-lg-strong'>
+				<Auth />
+			</Container>
+		</section>
+	)
 }
 
 export default Page
---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,5 @@
+const Auth = () => {
+	return <div className='md:grid md:grid-cols-2 md:gap-lg-strong'></div>
+}
+
+export default Auth
