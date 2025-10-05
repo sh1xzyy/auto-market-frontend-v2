2b118ae1a3e208e07dfec7ca11f734585f24d505
sh1xzyy <alekslungol@gmail.com>
2025-10-05 12:47:14 +0300

    feat: add auth card component and Divider component

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,5 +1,11 @@
+import { AuthCard } from '@/modules/AuthCard'
+
 const Auth = () => {
-	return <div className='md:grid md:grid-cols-2 md:gap-lg-strong'></div>
+	return (
+		<div className='md:grid md:grid-cols-2 md:gap-lg-strong'>
+			<AuthCard />
+		</div>
+	)
 }
 
 export default Auth
