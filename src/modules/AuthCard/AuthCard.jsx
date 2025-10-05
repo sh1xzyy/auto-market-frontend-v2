8a3255e3f17b9dee6c7c0726c569319595b5ccf6
sh1xzyy <alekslungol@gmail.com>
2025-10-05 12:57:52 +0300

    feat: add auth title

--- src/modules/AuthCard/AuthCard.jsx
+++ src/modules/AuthCard/AuthCard.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import AuthTabs from './ui/AuthTabs'
+import AuthTitle from './ui/AuthTitle'
 
 const AuthCard = () => {
 	const [activeIndex, setActiveIndex] = useState(0)
@@ -10,6 +11,15 @@ const AuthCard = () => {
 		<div>
 			<div className='mb-xl-plus bg-black-soft-bg border border-grey-deep md:rounded-lg'>
 				<AuthTabs activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+				<div className='px-lg-strong py-xl-plus md:p-3xl'>
+					<AuthTitle
+						title={
+							activeIndex === 0
+								? 'Hallo! Willkommen zurück!'
+								: 'Erstelle Dein mobile.de Konto!'
+						}
+					/>
+				</div>
 			</div>
 		</div>
 	)
---
+++ src/modules/AuthCard/ui/AuthTitle.jsx
@@ -0,0 +1,5 @@
+const AuthTitle = ({ title }) => {
+	return <h2 className='mb-lg-strong text-xl font-extrabold'>{title}</h2>
+}
+
+export default AuthTitle
